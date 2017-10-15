0bbd8a96ae878c799ce18ed2c324f710a5fcd40b
Greg Pfaff <greg.pfaff@razorfish.com>
2017-10-15 14:40:04 -0500

    adding updates to app

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,20 +1,33 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Loader extends Component {
+const Loader = (props) => (
+  <div className="loader">
+    <img src="/img/fleur.svg" alt="Loading..." />
+    <h2>{props.message}</h2>
+  </div>
+);
 
-  static propTypes = {
-    message: PropTypes.string.isRequired
-  }
-
-  render() {
-    return (
-      <div className="loader">
-        <img alt="Waiting to Load" src="./img/fleur.svg"/>
-        <h2>{this.props.message}</h2>
-      </div>
-    )
-  }
-}
+Loader.propTypes = {
+  message: PropTypes.string
+};
 
 export default Loader;
+
+// class Loader extends Component {
+
+//   static propTypes = {
+//     message: PropTypes.string.isRequired
+//   }
+
+//   render() {
+//     return (
+//       <div className="loader">
+//         <img alt="Waiting to Load" src="./img/fleur.svg"/>
+//         <h2>{this.props.message}</h2>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Loader;
